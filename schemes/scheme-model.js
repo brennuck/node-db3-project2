1cdd87f0f6b8126de9ac08393f3c5904c38687d9
Brennon Nuckols <brennuck@gmail.com>
2020-06-02 14:04:36 -0600

    update

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -5,8 +5,8 @@ module.exports = {
   findById,
   findSteps,
   add,
-//   update,
-//   remove,
+  update,
+  //   remove,
 };
 
 function find() {
@@ -18,12 +18,16 @@ function findById(id) {
 }
 
 function findSteps(id) {
-    return db('steps')
-    .join('schemes', 'schemes.id', 'steps.scheme_id',)
-    .select('schemes.scheme_name', 'steps.step_number', 'steps.instructions', )
+  return db("steps")
+    .join("schemes", "schemes.id", "steps.scheme_id")
+    .select("schemes.scheme_name", "steps.step_number", "steps.instructions")
     .where({ scheme_id: id });
 }
 
 function add(scheme) {
-    return db('schemes').insert(scheme, "id")
+  return db("schemes").insert(scheme, "id");
+}
+
+function update(changes, id) {
+  return db("schemes").where({ id }).update(changes);
 }
