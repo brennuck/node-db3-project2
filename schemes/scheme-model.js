6a5ce180db5bc92942835cda6c6016d6096dea66
Brennon Nuckols <brennuck@gmail.com>
2020-06-02 13:43:42 -0600

    findById

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,14 +1,18 @@
-const db = require('../data/db-config.js');
+const db = require("../data/db-config.js");
 
 module.exports = {
-    find,
-    findById,
-    findSteps,
-    add,
-    update,
-    remove,
-}
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
+};
 
 function find() {
-    return db("schemes")
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes").where({ id }).first();
 }
